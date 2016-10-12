d92d92890d0c67302f3922f2010837178525671d
Manu-sh <chilioslinux@gmail.com>
2016-10-12 16:56:05 +0200

    add install script

--- install-jmm.sh
+++ install-jmm.sh
@@ -5,3 +5,9 @@ default_path=/opt/javamm
 mkdir -p ${default_path} || exit 1
 [ -e "${1}" ] && unzip "${1}" -d ${default_path} || exit 1
 cp -fv javamm.desktop /usr/share/applications/javamm.desktop || exit 1
+
+
+! which xdg-user-dir && echo "please install xdg-user-dir for create symlink on desktop" && exit
+
+DESKTOP=$(xdg-user-dir DESKTOP)
+cp -fv javamm.desktop $DESKTOP/javamm.desktop || exit 1
